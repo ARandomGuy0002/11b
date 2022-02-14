099d5185bb8bea1aaeb34c0af8b03a0eeb85e211
ARandomGuy0002 <rankoma21@gmail.com>
2022-02-14 09:35:13 +0200

    gazq babi

---
+++ Lists/zad 1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace zad_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int firstNum = nums[0];
+            while (nums.Contains(firstNum))
+            {
+                nums.Remove(firstNum);
+            }
+            Console.WriteLine(string.Join(" ", nums));
+        }
+    }
+}
