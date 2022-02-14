64a2ce663325c01c3e6ded8513e8a05b1911aa61
ARandomGuy0002 <rankoma21@gmail.com>
2022-02-14 10:50:06 +0200

    butam babi 3

---
+++ Lists/zad 3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace zad_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            int counter = 0;
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+                if (arr[i] > 5)
+                {
+                    counter += 1;
+                }
+                if (arr[i] % 2 == 0)
+                {
+                    sum += arr[i];
+                }
+            }
+            Console.WriteLine($"count = {counter}");
+            Console.WriteLine($"sum = {sum}");
+            Console.WriteLine($"average = {arr.Average()}");
+        }
+    }
+}
