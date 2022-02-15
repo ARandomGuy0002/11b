ed9ea7fda412c0d55207564f046842bb51326ade
ARandomGuy0002 <rankoma21@gmail.com>
2022-02-15 12:29:08 +0200

    gazq babi 4

---
+++ izpitvane/zad 1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace zad_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] % 2 != 0)
+                {
+                    sum += arr[i];
+                }
+            }
+            Console.WriteLine($"sum = {sum}");
+            Array.Reverse(arr);
+            Console.WriteLine(string.Join(" ", arr));
+        }
+    }
+}
---
+++ izpitvane/zad 2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace zad_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = Console.ReadLine().Split().ToList();
+            string[] a = Console.ReadLine().Split().ToArray();
+            while (true)
+            {
+                if (a[0] == "stop")
+                {
+                    Console.WriteLine(string.Join(" ", names));
+                    break;
+                }
+                else if (a[0] == "find")
+                {
+                    if (names.Contains(a[2]) == false)
+                    {
+                        names.Insert(int.Parse(a[1]), a[2]);
+                    }
+                }
+                else if (a[0] == "add")
+                {
+                    int b = int.Parse(a[1]);
+                    while (b != 0)
+                    {
+                        names.Add(a[2]);
+                        b -= 1;
+                    }
+                }
+                else if (a[0] == "del")
+                {
+                    names.Remove(a[1]);
+                }
+                a = Console.ReadLine().Split().ToArray();
+            }
+    }
+}
